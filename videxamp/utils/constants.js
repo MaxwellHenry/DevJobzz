2d076d8d301aed96c658d9dc49f18627779132f1
Maxwell Montegary <maxwellmontegary@gmail.com>
2021-04-28 20:49:21 -0700

    Module 1 attempt 3

--- videxamp/utils/constants.js
+++
@@ -1,9 +0,0 @@
-const API_KEY = "meatballmarinara";
-const VALUE = "420 hah";
-const FUNC = () => console.log ('HELLO');
-
-module.exports = {
-    API_KEY, // equivalent to API_KEY: API_KEY
-    VALUE,
-    FUNC
-}
