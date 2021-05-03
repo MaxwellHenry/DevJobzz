83927b571a8250f38a4822b78d49b4ac132b2673
Maxwell Montegary <maxwellmontegary@gmail.com>
2021-05-02 19:14:37 -0700

    Module_03 Finished

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
+
+require('dotenv').config(); // this will read the .env file, if it exists
+
+const { PORT = 3000, WEATHER_KEY } = process.env;
+
+const WEATHER_KEY = "2c59ba50618bc82c4a8d626cde56abb5";
+
 console.log("Hello, world!");
+
 
 const express = require('express');
 const server = express();
@@ -11,6 +19,24 @@ server.use(express.static('public'));
 const bodyParser = require('body-parser');
 server.use(bodyParser.json());
 server.use(bodyParser.urlencoded({ extended: false }));
+
+const cowsay = require('cowsay');
+const Quote = require('inspirational-quotes')
+
+const WEATHER_KEY = process.env.WEATHER_KEY
+
+server.get('/cowspiration', (req, res) => {
+  const { text, author } = Quote.getQuote();
+
+  const cow = cowsay.say({
+    text: `${ text }\n\n- ${ author }`,
+    W: 80,
+  });
+
+  res.send({ cow });
+});
+
+const axios = require('axios');
 
 server.post('/job-search', async (req, res) => {
   try {
@@ -30,21 +56,26 @@ server.post('/job-search', async (req, res) => {
   }
 });
 
-// somewhere before server.listen()
-const cowsay = require('cowsay');
-const Quote = require('inspirational-quotes')
-
-server.get('/cowspiration', (req, res) => {
-  const { text, author } = Quote.getQuote();
-
-  const cow = cowsay.say({
-    text: `${ text }\n\n- ${ author }`,
-    W: 80,
-  });
-
-  res.send({ cow });
+server.get('/weather', async (req, res) => {
+  try {
+    const { lat, lon } = req.query;
+    const URL = `https://api.openweathermap.org/data/2.5/onecall?lat=${ lat }&lon=${ lon }&appid=${ WEATHER_KEY }`;
+    
+    const { data } = await axios.get(URL);
+    res.send({ results: data });
+    // make call, return data
+  } catch (error) {
+    res.send({ error });
+  }
+  
+  // make axios request
+  // send back data, or send back error
 });
 
+
+server.listen(3000, () => {
+  console.log('I am listening...');
+});
 
 server.get('/hello', (req, res, next) => {
     res.send(`
@@ -57,12 +88,15 @@ server.get('/hello', (req, res, next) => {
     `)
 });
 
-const axios = require('axios');
+const { PORT = 3000 } = process.env;
 
-
-
-
-
-server.listen(3000, () => {
-  console.log('I am listening...');
+server.listen(PORT, () => {
+  // stuff
 });
+
+
+
+
+
+
+
--- public/app.js
+++ public/app.js
@@ -32,6 +32,43 @@ async function fetchQuote() {
   $('#results').empty().append($(`<pre>${ cow }</pre>`))
 }
 
+async function fetchWeather() {
+  if (!navigator || !navigator.geolocation) {
+    $('#weather').append($('<h3>Weather not available on this browser</h3>'))
+  }
+
+  navigator.geolocation.getCurrentPosition(async (position) => {
+    const { coords: { latitude, longitude } } = position;
+
+    const response = await fetch(`/weather?lat=${ latitude }&lon=${ longitude }`);
+    const { results } = await response.json();
+
+    if (results.daily) {
+      $('#weather').empty();
+
+      results.daily.forEach(day => {
+        const { weather: [{ icon }] } = day;
+
+        $('#weather').append($(`
+          <img src="http://openweathermap.org/img/wn/${ icon }@2x.png" />
+        `));
+      });
+    }
+  }, async (error) => {
+    const result = await navigator.permissions.query({ name: 'geolocation' });
+    if (result.state == "denied") {
+      $('#weather').html(
+        $(`<div>
+            <h3>You have denied access to location services.</h3>
+            <h4>If you wish to see your forecast, update your settings and refresh.</h4>
+          </div>`)
+      )
+    }
+  });
+}
+
+fetchWeather();
+
 fetchQuote();
 
 
